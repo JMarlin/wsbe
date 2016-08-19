9d81fd831c2f6f78bc182d3f318be117acccff9e
Joseph Marlin <j.marlin@outlook.com>
2016-08-19 14:41:54 -0400

    Create current_build.js

---
+++ current_build.js
@@ -0,0 +1,1 @@
+alert("Looks like you just got a fresh clone of the git repo. Make sure you have Emscripten installed on your system and then run a build script from one of the chapter folders in an Emscripten-aware terminal and refresh this page to see the output of that example!");
